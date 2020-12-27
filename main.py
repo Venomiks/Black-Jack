d53130dd40ececfef18dc2c37a865b4752bd8576
Venomiks <73553059+Venomiks@users.noreply.github.com>
2020-12-27 18:21:51 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import random
 import os
 
-cards = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 cards_in_hand = [random.choice(cards),random.choice(cards),random.choice(cards)]
-#while True:
-play= input("would you like to play Black Jack?")
-if play == 'yes':
-    print(cards_in_hand)
-
-#if random.choice(cards) == 11:
-#    print('J')
-
-
-    choice = input("Add another card?")
-    if choice == 'yes':
-        cards_in_hand.append(random.choice(cards))
+while True:
+    play= input("would you like to play Black Jack?")
+    if play == 'yes':
         print(cards_in_hand)
     else:
-        pass
+        exit()
+
+
+        choice = input("Add another card?")
+        if choice == 'yes':
+            cards_in_hand.append(random.choice(cards))
+            print(cards_in_hand)
+        else:
+            pass
 
     sum_of_cards = sum(cards_in_hand)
 
@@ -26,5 +25,6 @@ if play == 'yes':
     else:
         print("You lose")
 
-# Play_again = print('Play again?')
- # if Play_again == 'Yes':
+    answer = input(" Wanna play again? yes/no: \n")
+    if answer != 'yes':
+        breakpoint()
